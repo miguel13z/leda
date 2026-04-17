798aa8ea218d140c2efb1b008da618e94ef5cd47
miguel13z <miguelfariasmelo@gmail.com>
2026-04-16 23:55:01 -0300

    Commit inicial: exercícios e análise empírica de algoritmos

---
+++ exercicios/inserePrimeiro.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class inserePrimeiro{
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String[] linha = sc.nextLine().split(" ");
+        sc.close();
+
+        int[] numeros = transformaInt(linha);
+        for (int i = 0; i < numeros.length - 1; i++){
+            if (numeros[i] > numeros[i + 1]){
+                int temp = numeros[i];
+                numeros[i] = numeros[i + 1];
+                numeros[i + 1] = temp;
+            }
+            else {
+                break;
+            }
+        }
+
+        System.out.println(Arrays.toString(numeros));
+    }
+
+    public static int[] transformaInt(String[] linha){
+        int[] numeros = new int[linha.length];
+        for (int i = 0; i < linha.length; i++){
+            numeros[i] = Integer.parseInt(linha[i]);
+        }
+
+        return numeros;
+    }
+}
---
+++ exercicios/insereUltimo.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class insereUltimo{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String[] linha = sc.nextLine().split(" ");
+        sc.close();
+        
+        int[] numeros = transformaInt(linha);
+        for(int i = numeros.length - 1; i >= 1; i--){
+            if (numeros[i] < numeros[i - 1]){
+                int temp = numeros[i];
+                numeros[i] = numeros[i - 1];
+                numeros[i - 1] = temp;
+            }
+            else {
+                break;
+            }
+        }
+
+        System.out.println(Arrays.toString(numeros));
+    }
+
+    public static int[] transformaInt(String[] linha){
+        int[] numeros = new int[linha.length];
+        for (int i = 0; i < linha.length; i++){
+            numeros[i] = Integer.parseInt(linha[i]);
+        }
+
+        return numeros;
+    }
+}
