b42d450a498923f1154dbdc6eb54c10c70387abd
20250019643 MIGUEL FARIAS MELO <miguel.farias.melo@lcc3-003.lcc.ufcg.edu.br>
2026-06-10 09:09:58 -0300

    fazendo funções recursivas na LinkedList

--- template-linkedlist/src/main/java/MeusTestes.java
+++ template-linkedlist/src/main/java/MeusTestes.java
@@ -10,19 +10,11 @@ public class MeusTestes {
         list.addLast(3);
         list.addLast(4);
         list.addLast(5);
+        list.addLast(10);
 		
         System.out.println(list.toString() + "\n");
         
-        Node n1 = list.getNo(1);
-        Node n2 = list.getNo(2);
-        list.swap(n1, n2);
-        
-        
-        System.out.println(list.toString() + "\n");
-        
-        list.addLast(20);
-        
-        System.out.println(list.toString() + "\n");
+        System.out.println(list.getNo(0).value);
 	}
 
 }
