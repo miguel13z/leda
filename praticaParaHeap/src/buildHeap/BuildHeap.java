30702fe51ac2bbcee8ef89a3d8ddbf3d7394b6c7
20250019643 MIGUEL FARIAS MELO <miguel.farias.melo@lcc2-53.lcc.ufcg.edu.br>
2026-07-09 11:45:13 -0300

    ajeitando build heap

--- praticaParaHeap/src/buildHeap/BuildHeap.java
+++ praticaParaHeap/src/buildHeap/BuildHeap.java
@@ -7,14 +7,15 @@ public class BuildHeap {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String[] entrada = sc.nextLine().split(" ");
+		sc.close();
 		int[] v = converteEntrada(entrada);
 		
 		System.out.println(buildHeap(v));
 	}
 
-	private static char[] buildHeap(int[] v) {
-		// TODO Auto-generated method stub
-		return null;
+	private static String buildHeap(int[] v) {
+		Heap tree = new Heap(v);
+		return tree.toString();
 	}
 
 	private static int[] converteEntrada(String[] entrada) {
@@ -38,41 +39,39 @@ class Heap {
 	}
 	
 	public Heap(int[] array) {
-		
+		this.heap = array;
+		buildHeap(this.heap);
 	}
 	
-	public void buildHeap() {
-		
+	public void buildHeap(int[] v) {
+		int iPai = getIndicePai(v.length - 1);
+		for (int i = iPai; i >= 0; i--) {
+			heapify(i);
+		}
+	}
+	
+	@Override
+	public String toString() {
+		return Arrays.toString(heap);
 	}
 	
 	public void heapify(int i) {
-		int iFilhoEsq = getIndiceFilhoEsq(i);
-		int iFilhoDir = getIndiceFilhoDir(i);
-		if (isIndiceValido(iFilhoEsq) && isIndiceValido(iFilhoDir)) {
-			int maior = Math.max(heap[i], Math.max(heap[iFilhoEsq], heap[iFilhoDir]));
-			
-			if (maior == heap[iFilhoDir]) {
-				swap(i, iFilhoDir);
-				heapify(iFilhoDir);
-			}
-			if (maior == heap[iFilhoEsq]) {
-				swap(i, iFilhoEsq);
-				heapify(iFilhoEsq);
-			}
-		} else if (isIndiceValido(iFilhoEsq)) {
-			int maior = Math.max(heap[i], heap[iFilhoEsq]);
-			
-			if (maior == heap[iFilhoEsq]) {
-				swap(i, iFilhoEsq);
-				heapify(iFilhoEsq);
-			}
-		} else if (isIndiceValido(iFilhoDir)) {
-			int maior = Math.max(heap[i], heap[iFilhoEsq]);
-			
-			if (maior == heap[iFilhoDir]) {
-				swap(i, iFilhoDir);
-				heapify(iFilhoDir);
-			}
+		int maior = i;
+		
+	   	int left = getIndiceFilhoEsq(i);
+		int right = getIndiceFilhoDir(i);
+		
+		if (isIndiceValido(left) && heap[left] > heap[maior]) {
+			maior = left;
+		}
+		
+		if (isIndiceValido(right) && heap[right] > heap[maior]) {
+			maior = right;
+		}
+		
+		if (maior != i) {
+			swap(i, maior);
+			heapify(maior);
 		}
 	}
 	
@@ -83,7 +82,7 @@ class Heap {
 	}
 	
 	public int getIndicePai(int i) {
-		
+		return (i - 1) / 2;
 	}
 	
 	private int getIndiceFilhoEsq(int i) {
